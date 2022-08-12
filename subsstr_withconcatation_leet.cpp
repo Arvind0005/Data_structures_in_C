027ffa83acc8e540af40444ff44efd731a7f2876
Arvind_Suresh <53540697+Arvind0005@users.noreply.github.com>
2022-08-13 00:44:02 +0530

    Update subsstr_withconcatation_leet.cpp

--- subsstr_withconcatation_leet.cpp
+++ subsstr_withconcatation_leet.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
     vector<int> findSubstring(string s, vector<string>& words) {
-        if(s.length==0 ||)
-       int wlen = words[0].length();
+         vector<int> arr;
+        if(s.length()==0 || words.size()==0)
+        {
+            return arr;
+        }
+        
+        int wlen = words[0].length();
         int nw = words.size();
         unordered_map<string,int> wordf;
-        vector<int> arr;
+       
         for(int i=0;i<nw;i++)
         {
             wordf[words[i]]++;
@@ -17,25 +22,26 @@ public:
             unordered_map<string,int> word;
             int nwords=0;
             int dj;
-            if(i==s.length()-(nw*wlen)-1)
-            {
-                dj=i+1;
-            }
-            else
-            {
-                dj=i;
-            }
+            dj=i;
             for(int j=dj;j<i+wlen;j++)
             {
                 sword.push_back(s[j]);
             }
-            cout<<sword<<" "<<wordf[sword]<<"\n";
+           
             if(wordf[sword]>=1)
             {
-                //cout<<"hii1";
+                
                 word[sword]++;
                 nwords++;
                 int ni=i;
+                if(nwords==nw)
+                {
+                    if(i==s.length()-(nw*wlen)+1)
+                            arr.push_back(i+1);
+                    else
+                        arr.push_back(i);
+                 
+                }
                 while(true)
                 {
                     ni = ni+wlen;
@@ -46,23 +52,29 @@ public:
                     }
                     if(!(wordf[sword]>=1))
                     {
-                        cout<<"hii1 "<<sword<<" "<<wordf[sword]<<"\n";
+                       
                         break;
                     }
-                    cout<<sword<<" "<<word[sword]<<"\n";
+                   
                     word[sword]=word[sword]+1;
                     if(word[sword]>wordf[sword])
                     {
-                        cout<<"hii2 "<<sword<<" "<<wordf[sword]<<" "<<word[sword]<<"\n";
+                     
                         break;
                     }
                     nwords++;
                     if(nwords==nw)
                     {
-                        if(i==s.length()-(nw*wlen)-1)
+                        if(i==s.length()-(nw*wlen)+1)
+                        { 
+                       
                             arr.push_back(i+1);
+                        }
                         else
+                        {
+                            cout<<"hello3"<<i<<"\n";
                             arr.push_back(i);
+                        }
                         break;
                     }
                 }
