2613c0ec709e4749c92746823c3d93072bb87428
Arvind_Suresh <53540697+Arvind0005@users.noreply.github.com>
2022-10-11 13:40:52 +0530

    Create climbing_stairs_leet.java

---
+++ climbing_stairs_leet.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int climbStairs(int n) {
+        int one=1,two=1;
+        for(int i=0;i<n-1;i++)
+        {
+            int temp=one;
+            one=two+one;
+            two=temp;
+        }
+        return one;
+    }
+}
+
