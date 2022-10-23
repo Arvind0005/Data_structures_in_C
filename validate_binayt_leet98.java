50a1dd33122534761587a4d90ff12a16990f813b
Arvind_Suresh <53540697+Arvind0005@users.noreply.github.com>
2022-10-24 01:51:37 +0530

    Create  validate_binayt_leet98.java

---
+++ validate_binayt_leet98.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        if(root==null)
+            return true;
+        return dfs(root,null,null);
+        
+    }
+    public boolean dfs(TreeNode root,Integer min,Integer max)
+    {
+        if(root==null)
+            return true;
+        if((min!=null && root.val<=min) ||(max!=null &&root.val>=max))
+            return false;
+        return (dfs(root.left,min,root.val) && dfs(root.right,root.val,max));
+    }
+}
+Console
